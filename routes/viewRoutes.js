e3592e175bfbe20dc9635e2b72c6364a434f01db
yasharjit <yasharjitm10@gmail.com>
2021-05-02 14:02:19 +0530

    Refactor code for project catalog

---
+++ routes/viewRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    return res.render("index", {
+        title: "Home",
+    });
+});
+
+router.get("/login", (req, res) => {
+    return res.render("login", {
+        title: "Login",
+    });
+});
+
+router.get("/signup", (req, res) => {
+    return res.render("signup", {
+        title: "Signup",
+    });
+});
+
+router.get("/about", (req, res) => {
+    return res.render("about", {
+        title: "About",
+    });
+});
+
+router.get("/projectCatalog", (req, res) => {
+    return res.render("projectCatalog", {
+        title: "Project Catalog",
+    });
+});
+
+module.exports = router;

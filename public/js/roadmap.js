f19700f2405a0c519e0d096ef135da6467c0eccf
yasharjit <yasharjitm10@gmail.com>
2021-11-30 17:18:18 +0530

    assign variables

---
+++ public/js/roadmap.js
@@ -0,0 +1,5 @@
+function assignVariables() {
+    var select = document.getElementById("presentSkillLevel");
+    var option = select.options[select.selectedIndex].value;
+    console.log("hello");
+}

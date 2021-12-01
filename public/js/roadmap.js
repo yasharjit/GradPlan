20f7413e499aa254bce7316f3db3e249844d4869
Anmolgarg123 <smahajan3_be18@thapar.edu>
2021-12-01 16:37:18 +0530

    js error resolved

--- public/js/roadmap.js
+++ public/js/roadmap.js
@@ -1,5 +1,10 @@
-function assignVariables() {
-    var select = document.getElementById("presentSkillLevel");
-    var option = select.options[select.selectedIndex].value;
-    console.log("hello");
+// function assignVariables() {
+//     var select = document.getElementById("presentSkillLevel");
+//     var option = select.options[select.selectedIndex].value;
+//     console.log("hello");
+// }
+document.getElementById("submitted").addEventListener("click", myFunction);
+function myFunction() {
+    alert("sfsfs");
+    console.log("wewewe");
 }

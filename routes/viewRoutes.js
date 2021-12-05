0771ffc190e7992a5e5173385b25f29281c43e51
yasharjit <yasharjitm10@gmail.com>
2021-12-05 19:03:13 +0530

    search backend working

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -95,6 +95,25 @@ router.get(
     })
 );
 
-router.get("/search", viewController.autocomplete);
+router.get(
+    "/search",
+    catchAsync(async (req, res) => {
+        let q = req.query.projectName;
+        Project.find(
+            {
+                projectName: {
+                    $regex: new RegExp(q),
+                },
+            },
+            // {
+            //     _id: 0,
+            //     __v: 0,
+            // },
+            function (err, data) {
+                res.json(data);
+            }
+        ).limit(10);
+    })
+);
 
 module.exports = router;

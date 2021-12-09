395e90734e4e75ae6f33b504ad43115f0878f105
vartika <vartika.singh2022@gmail.com>
2021-12-09 16:23:49 +0530

    array for every feature of roadmap created

--- public/js/roadmap.js
+++ public/js/roadmap.js
@@ -1,4 +1,89 @@
-const Roadmap = [
+const FrontEndRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "https://www.youtube.com/watch?v=QA0XpGhiz5w&ab_channel=DesignCourse"
+    },
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "https://www.coursera.org/learn/html-css-javascript-for-web-developers"
+    },
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "https://www.youtube.com/watch?v=yoFsTaQEt34&ab_channel=Simplilearn"
+    },
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0"
+    },
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "https://www.udemy.com/course/front-end-web-development/?utm_source=adwords&utm_medium=udemyads&utm_campaign=WebDevelopment_v.PROF_la.EN_cc.INDIA_ti.8322&utm_content=deal4584&utm_term=_._ag_82381207618_._ad_533094292056_._kw__._de_c_._dm__._pl__._ti_dsa-774930032289_._li_9301805_._pd__._&matchtype=&gclid=CjwKCAiAhreNBhAYEiwAFGGKPD6YL__BJwRHHsGAK_Yq6vAGVigXFWNs627hmrD6WhlK1TyhKYAYLBoCW4YQAvD_BwE"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "https://developer.mozilla.org/en-US/docs/Learn/Front-end_web_developer"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "https://www.youtube.com/watch?v=Q8NPQ2RgWyg&ab_channel=Simplilearn"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "https://exlskills.com/learn-en/courses/advanced-skills-to-learn-to-become-an-ace-front-end-developer-advanced_skills_for_fe_developer"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "https://www.frontendmentor.io/challenges/fylo-dark-theme-landing-page-5ca5f2d21e82137ec91a50fd"
+    }
+];
+const BackendRoadmap = [
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
+const FullStackRoadmap = [
     {
         skill: "frontend",
         lvl: "beginner",
@@ -30,6 +115,205 @@ const Roadmap = [
         path: "hello6"
     }
 ];
+
+const AppDevRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "frontend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
+const MLRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "frontend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
+const CppRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "frontend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
+const JavaRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "frontend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
+const PythonRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "frontend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
+const GameDevRoadmap = [
+    {
+        skill: "frontend",
+        lvl: "beginner",
+        path: "hello1"
+    },
+    {
+        skill: "frontend",
+        lvl: "intermediate",
+        path: "hello2"
+    },
+    {
+        skill: "frontend",
+        lvl: "advanced",
+        path: "hello3"
+    },
+    {
+        skill: "backend",
+        lvl: "beginner",
+        path: "hello4"
+    },
+    {
+        skill: "backend",
+        lvl: "intermediate",
+        path: "hello5"
+    },
+    {
+        skill: "backend",
+        lvl: "advanced",
+        path: "hello6"
+    }
+];
+
 
 var finalPath;
 
